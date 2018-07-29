7f3b9c819a8f411c52cc7bf3ef39c3b96e939441
WincerChan <wincerchan@gmail.com>
2018-07-29 20:42:37 +0800

    :sparkles: Add HttpServer

--- server.py
+++ server.py
@@ -5,6 +5,25 @@ class Signal:
     go = True
 
 
+def parse_url(url)->str:
+    params = {}
+    params['Transfer-Encoding'] = 'chunked'
+    params['Content-Type'] = 'text/plain;'
+    params['Date'] = 'Sun, 29 Jul 2018 00:30:00 GMT'
+    params['Server'] = 'Python'
+    return params
+
+
+def transfer_encoding(func):
+    def chunked(*args):
+        result, conn = func(*args)
+        lens = hex(len(result))[2:]
+        first = lens.encode('ascii') + b'\r\n'
+        last = b'\r\n0\r\n\r\n'
+        conn.send(first + result + last)
+    return chunked
+
+
 class EchoServer:
     def __init__(self, port=8888, addr='0.0.0.0', family=AF_INET,
                  type_=SOCK_STREAM, backlog=5, init=True):
@@ -41,7 +60,45 @@ class EchoServer:
         self._run()
 
 
-serv = EchoServer(port=5178)
+class HttpServer(EchoServer):
+    @transfer_encoding
+    def _get_body(self, conn) -> str:
+        body = 'Hello World'.encode('ascii')
+        return body, conn
+
+    def _get_head(self, head):
+        params = parse_url(head)
+        headers = 'HTTP/1.1 200\r\n'
+        for key, value in params.items():
+            if value:
+                headers += '{}: {}\r\n'.format(key, value)
+
+        return headers + '\r\n'
+
+    def _echo(self, sock):
+        while True:
+            try:
+                req_head = sock.recv(1024)
+                print('Received: ', req_head)
+            except BrokenPipeError:
+                break
+            else:
+                if not req_head:
+                    break
+                if req_head.startswith(b'GET'):
+                    head = self._get_head(req_head)
+                    sock.send(head.encode('ascii'))
+                    print(head.encode('ascii'))
+                    self._get_body(sock)
+
+    def __call__(self):
+        self.sock = socket(self.family, self.type_)
+        self.sock.bind((self.addr, self.port))
+        print('Listen in %s porn.' % self.port)
+        self._run()
+
+
+serv = HttpServer(port=5174)
 
 try:
     serv()
